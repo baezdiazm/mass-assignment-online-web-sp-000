d0b75a34684ca043b1934be9e24301f89c4dbb6b
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-12-10 21:27:10 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,19 +2,22 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(arg)
-    @name = name
-    @birthday = birthday
-    @hair_color = hair_color
-    @eye_color = eye_color
-    @height = height
-    @weight = weight
-    @handed = handed
-    @complexion = complexion
-    @t_shirt_size = t_shirt_size
-    @wrist_size = wrist_size
-    @glove_size = glove_size
-    @pant_length = pant_length
-    @pant_width = pant_width
+    arg.each {|k, v|
+      self.send("#{k}=", v)
+    }
+#    @name = name
+#    @birthday = birthday
+#    @hair_color = hair_color
+#    @eye_color = eye_color
+#    @height = height
+#    @weight = weight
+#    @handed = handed
+#    @complexion = complexion
+#    @t_shirt_size = t_shirt_size
+#    @wrist_size = wrist_size
+#    @glove_size = glove_size
+#    @pant_length = pant_length
+#    @pant_width = pant_width
   end
 
 end
